3f60510cd02db1510695987bf9b0ee431dafd5e6
Clicia Pinto <cliciasp1@gmail.com>
2016-08-11 15:37:59 -0300

    Adicionando nova funcionalidade ao projeto

---
+++ hellonewworld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.>
+
+int main(){
+	int a, b, c;
+	for(int i=0; i<10; i++){
+		a = 2;
+	}
+	return 0;
+}
--- helloword.c
+++ helloword.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char const *argv[]){
+int main(){
 	printf("Hello World!\n");
 	printf("Faz paarte do projeto learning!!\n");
+	func1();
+	func2();
 	return 0;
 }
